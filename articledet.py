630204f2f4dac003c59134730aa392654d4d0a85
salik03 <uddinsalik03@gmail.com>
2023-09-23 18:12:38 +0530

    articledet made into api

--- articledet.py
+++ articledet.py
@@ -1,28 +1,29 @@
+from flask import Flask, request, jsonify
 import pandas as pd
 
+app = Flask(__name__)
 
+# Initialize data
 csv_file_name = "ArticleData.csv"
 try:
     existing_data = pd.read_csv(csv_file_name)
 except FileNotFoundError:
-    existing_data = None
+    existing_data = pd.DataFrame(columns=["AID", "Article", "NAME", "INSURED", "PRODUCT TYPE", "GROSS WT", "COUNTRY"])
 
 
-if existing_data is None:
-    data = pd.DataFrame(columns=["AID", "Article", "NAME", "INSURED", "PRODUCT TYPE", "GROSS WT", "COUNTRY"])
-else:
-    data = existing_data
+@app.route('/add_row', methods=['POST'])
+def add_row():
+    new_row = {}
+    for column in existing_data.columns:
+        value = request.json.get(column)
+        new_row[column] = value
+
+    global existing_data
+    existing_data = existing_data.append(new_row, ignore_index=True)
+    existing_data.to_csv(csv_file_name, mode='w', index=False)
+    
+    return jsonify({"message": "Data has been saved to {}".format(csv_file_name)})
 
 
-new_row = {}
-for column in data.columns:
-    value = input(f"Enter {column}: ")
-    new_row[column] = value
-
-
-data = data.append(new_row, ignore_index=True)
-
-
-data.to_csv(csv_file_name, mode='w', index=False)
-
-print(f"Data has been saved to {csv_file_name}")
+if __name__ == '__main__':
+    app.run(debug=True)
