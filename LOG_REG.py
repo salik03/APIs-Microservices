c1d3ae1640edf882ef378f7b1445e2569f61fcff
salik03 <uddinsalik03@gmail.com>
2023-09-23 17:30:06 +0530

    made some fixes

--- LOG_REG.py
+++ LOG_REG.py
@@ -1,77 +1,58 @@
 import csv
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
 column_names = ["EMAIL", "MOBILE NUMBER", "USERNAME", "PASSWORD"]
+csv_file = "user_data.csv"
+
 
 def create_csv_file():
-    
-    csv_file = "user_data.csv"
-
-    
     with open(csv_file, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(column_names)
 
-def register_user(email, mobile_number, username, password):
-    
-    csv_file = "user_data.csv"
 
-    
+def register_user(email, mobile_number, username, password):
     with open(csv_file, mode='a', newline='') as file:
         writer = csv.writer(file)
-
-      
         writer.writerow([email, mobile_number, username, password])
 
-def login_user(username, password):
-    
-    csv_file = "user_data.csv"
 
-    
+def login_user(username, password):
     with open(csv_file, mode='r') as file:
         reader = csv.reader(file)
         for row in reader:
-            saved_username, saved_password = row[2], row[3]  
+            saved_username, saved_password = row[2], row[3]
             if username == saved_username and password == saved_password:
                 return True
-
     return False
 
-def main():
-    
-    create_csv_file()
 
-    while True:
-        print("1. Register")
-        print("2. Login")
-        print("3. Exit")
-        
-        choice = input("Enter your choice: ")
-        
-        if choice == '1':
-            
-            email = input("Enter Email: ")
-            mobile_number = input("Enter Mobile Number: ")
-            username = input("Enter Username: ")
-            password = input("Enter Password: ")
-            
-            
-            register_user(email, mobile_number, username, password)
-            print("User registered successfully!")
-        
-        elif choice == '2':
-            
-            username = input("Enter Username: ")
-            password = input("Enter Password: ")
-            
-            
-            if login_user(username, password):
-                print("Login successful!")
-            else:
-                print("Login failed. Invalid username or password.")
-        
-        elif choice == '3':
-            break
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get('email')
+    mobile_number = data.get('mobile_number')
+    username = data.get('username')
+    password = data.get('password')
+    register_user(email, mobile_number, username, password)
+    return jsonify({"message": "User registered successfully!"})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if login_user(username, password):
+        return jsonify({"message": "Login successful!"})
+    else:
+        return jsonify({"message": "Login failed. Invalid username or password."})
+
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
