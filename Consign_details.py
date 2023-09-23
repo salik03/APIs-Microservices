1e16b36f4ebeee02fe7843176ccba92672634d0c
champ007644 <kushagrasingh6355@gmail.com>
2023-09-24 00:05:02 +0530

    commit

--- Consign_details.py
+++ Consign_details.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 
 try:
@@ -7,7 +8,7 @@ except FileNotFoundError:
     
     existing_data = pd.DataFrame(columns=[
         'Product type', 'Category of item', 'No. of Types of items in Consignment',
-        'Gross weight', 'Total value (INR)', 'SDR value (INR)', 'Instructions in case of Non-Delivery',
+        'Gross weight', 'Total value (INR)', 'Instructions in case of Non-Delivery',
         'No. of Licenses', 'No. of Invoices', 'No. of Certificates', 'SELECT PBE', 'Self filling', 'Senders Custom Ref No', 'Receivers Import Ref No'
     ])
 
@@ -18,7 +19,6 @@ new_data['Category of item'] = [input("Enter Category of item: ")]
 new_data['No. of Types of items in Consignment'] = [int(input("Enter No. of Types of items in Consignment: "))]
 new_data['Gross weight'] = [float(input("Enter Gross weight: "))]
 new_data['Total value (INR)'] = [float(input("Enter Total value (INR): "))]
-new_data['SDR value (INR)'] = [float(input("Enter SDR value (INR): "))]
 new_data['Instructions in case of Non-Delivery'] = [input("Enter Instructions in case of Non-Delivery: ")]
 new_data['No. of Licenses'] = [int(input("Enter No. of Licenses: "))]
 new_data['No. of Invoices'] = [int(input("Enter No. of Invoices: "))]
@@ -28,6 +28,10 @@ new_data['Self filling'] = [input("Enter Self filling: ")]
 new_data['Senders Custom Ref No'] = [int(input("Enter Senders Custom Ref No: "))]
 new_data['Receivers Import Ref No'] = [int(input("Enter Senders Custom Ref No: "))]
 
+#SDR CALCULATOR
+new_data['SDR'] = new_data['Total value (INR)'].apply(lambda x: math.ceil(0.01 * x))
+
+#TARIFF CALCULATOR
 
 
 combined_data = pd.concat([existing_data, new_data], ignore_index=True)
@@ -36,3 +40,7 @@ combined_data = pd.concat([existing_data, new_data], ignore_index=True)
 combined_data.to_csv('consignment_data.csv', index=False)
 
 print("Data has been saved to consignment_data.csv")
+print(f"Calculated SDR value: {new_data['SDR'].values[0]}")
+
+
+
